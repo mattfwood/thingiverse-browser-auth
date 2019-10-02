243ddbe3de9f7f79f8ac4315c27d198024f2589e
Matt Wood <matt@cratebind.com>
2019-10-01 20:45:29 -0500

    Update prod config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 module.exports = {
-  host: process.env.NODE_ENV === 'production' ? 'https://thingiverse-browser.herokuapp.com/' : 'http://localhost:3000/',
+  host: process.env.NODE_ENV === 'production' ? 'https://thingiverse-browser.now.sh/' : 'http://localhost:3000/',
   thingiverse_client_id: 'ac58b9e55dd80f6834f6',
 }
