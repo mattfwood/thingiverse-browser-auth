2141056c3289df70d99a0d7352b33b8dea5ffcd6
Matt Wood <matt@cratebind.com>
2019-10-07 09:57:27 -0500

    Add nprogress bar

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,19 @@
-import React from 'react'
-import { ThemeProvider, CSSReset } from "@chakra-ui/core";
+import React from 'react';
+import { ThemeProvider, CSSReset } from '@chakra-ui/core';
+import NProgress from 'nprogress';
+import Router from 'next/router';
+import Head from 'next/head';
 
-import App from 'next/app'
+import App from 'next/app';
+
+// show nprogress loading bar on route change
+Router.events.on('routeChangeStart', url => {
+  console.log(`Loading: ${url}`);
+  NProgress.start();
+});
+
+Router.events.on('routeChangeComplete', () => NProgress.done());
+Router.events.on('routeChangeError', () => NProgress.done());
 
 class MyApp extends App {
   // Only uncomment this method if you have blocking data requirements for
@@ -17,14 +29,20 @@ class MyApp extends App {
   // }
 
   render() {
-    const { Component, pageProps } = this.props
+    const { Component, pageProps } = this.props;
+
     return (
-      <ThemeProvider>
-        <CSSReset />
-        <Component {...pageProps} />
-      </ThemeProvider>
-    )
+      <>
+        <Head>
+          <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
+        </Head>
+        <ThemeProvider>
+          <CSSReset />
+          <Component {...pageProps} />
+        </ThemeProvider>
+      </>
+    );
   }
 }
 
-export default MyApp
+export default MyApp;
